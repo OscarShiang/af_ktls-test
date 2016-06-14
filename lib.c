3ab0ca33759e2616a59689e263e68616f555bbbc
Lance Chao <lancerchao@gmail.com>
2016-06-14 15:25:56 -0700

    changed rand() to random()

--- lib.c
+++ lib.c
@@ -8,7 +8,7 @@ void gen_random(char *s, const int len) {
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
     for (int i = 0; i < len; ++i) {
-        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+        s[i] = alphanum[random() % (sizeof(alphanum) - 1)];
     }
     s[len] = 0;
 }
